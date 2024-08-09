b3f3a7f902a2211a486943208bcf8dc5af9809f9
randyfastcode <88994953+randyfastcode@users.noreply.github.com>
2024-08-09 18:27:42 -0400

    Delete index.test.js

--- index.test.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-
-test('La página HTML debe contener "Hola Mundo"', () => {
-    const htmlContent = fs.readFileSync('index.html', 'utf-8');
-    expect(htmlContent).toContain('Hola Mundo');
-});
